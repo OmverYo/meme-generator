bd857804752cfaad309895d8c245f93d281235fe
OmverYo <justinlee990106@gmail.com>
2023-02-09 20:14:25 +1300

    Major changes to input space and button styling

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,9 +2,12 @@ import React from "react"
 
 export default function Meme() {
     return (
-        <div className="meme">
-            <input></input>
-            <input></input>
-        </div>
+        <main>
+            <form className="form">
+                <input type="" placeholder="Top Text" className="form--input"/>
+                <input type="" placeholder="Bottom Text" className="form--input"/>
+                <button className="form--button">Get a new meme image</button>
+            </form>
+        </main>
     )
 }
