87e4d9b3c0402d57a0ba23e8f665d1a57e990e7f
OmverYo <justinlee990106@gmail.com>
2023-02-12 19:47:38 +1300

    Displaying random images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,19 +2,22 @@ import React from "react"
 import memesData from "./../memesData"
 
 export default function Meme(props) {
+    const [memeImage, setMemeImage] = React.useState("")
+
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        setMemeImage(memesArray[randomNumber].url)
     }
 
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Top Text" className="form--input"/>
                 <input type="text" placeholder="Bottom Text" className="form--input"/>
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
-            </form>
+            </div>
+            <img src={memeImage} className="meme--image" alt="MemeImage"/>
         </main>
     )
 }
