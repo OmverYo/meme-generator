97dba906de0ebb4b825768c89b6e779410793da7
OmverYo <justinlee990106@gmail.com>
2023-02-13 19:39:33 +1300

    Utilising new skills of Props and State

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,12 +2,23 @@ import React from "react"
 import memesData from "./../memesData"
 
 export default function Meme(props) {
-    const [memeImage, setMemeImage] = React.useState("")
-
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
     }
 
     return (
@@ -17,7 +28,7 @@ export default function Meme(props) {
                 <input type="text" placeholder="Bottom Text" className="form--input"/>
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
             </div>
-            <img src={memeImage} className="meme--image" alt="MemeImage"/>
+            <img src={meme.randomImage} className="meme--image" alt="MemeImage"/>
         </main>
     )
 }
